b2300bf299d9e06327d9bfd9665ba78ddbb4a305
Diego Armando Cruz Sepulveda <diegocruzsep@gmail.com>
2023-05-24 14:45:44 -0600

    Modificacion al index

--- index.php
+++ index.php
@@ -52,11 +52,36 @@
     </header>
 
     <main class="container mt-5">
-        <h1>Titulo de la página</h1>
+        <h1>PELICULAS</h1>
 
         <h2><?php if($auth){
             echo "Bienvenido " . $nombre;
         } ?></h2>
+
+        <div class="row">
+            <div class="col-md-6 col-lg-4 mb-4 w-1">
+                <img src="img/1.jpg" alt="">
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <img src="img/2.jpg" alt="">
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <img src="img/3.jpg" alt="">
+            </div>
+        </div>
+
+        <h2 class="mt-3" >LAS MEJORES PELICULAS DEL MOMENTO</h2>
+            <p>
+                Las mejores películas de 2023 nos dejaron algunas joyas imprescindibles y aquí las colocamos de peor a mejor para que sepas qué 
+                debes recuperar y qué debes olvidar.
+            </p>
+            <p>
+                La lista contiene películas que fueron tan esperadas como 'The Batman' con Robert Pattinson,
+                las aclamadas 'Drive my car' y 'La peor persona del mundo', y además de películas de Marvel como 'Black Panther: Wakanda Forever'.
+                En muchas de ellas los números hablaron por sí solos, de alguna de las películas de Netflix más vistas de su historia (que, claro,
+                ya se cuentan entre las mejores películas de Netflix en 2023) hasta algún taquillazo inesperado.
+            </p>
+    </main>
     </main>
    <div class="contenedor-imagen">
    
